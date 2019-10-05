1ae7aebcac14661700c0e9ac183844823768d392
Jonathan Eriksson <LandisGaming@hotmail.com>
2019-10-06 01:54:07 +0200

    Almost done

--- myBlackjack/Program.cs
+++ myBlackjack/Program.cs
@@ -8,51 +8,99 @@ namespace Blackjack
 {
     class Program
     {
-        static List<Player> players = new List<Player>();
+        static List<Player> players = new List<Player>(); //List of all players in game except dealer
         static Random randomInt = new Random();
-        static int dealerCards = 0;
-        static int maxNum = 0;
+        static int dealerCards = 0; //Sum of all of dealer's cards 
+        static int maxNum = 0; // Highest number out of all players not over 21.
+        static int selectedMenuInput = 0; //input for switch case.
         static bool playerNotDone = true;
         static bool dealerNotDone = true;
         static bool gameOver = false;
+        static bool exitGame = false;
+
 
         static void Main(string[] args)
         {
-            intializeGame();
-            playerDeal();
-            if (gameOver != true)
+            while(exitGame == false)
             {
-                dealerDeal();
+                GameMenu();
+                IntializeGame();
+                playerDeal();
+                if (gameOver != true)
+                {
+                    dealerDeal();
+                }
+                Console.ReadKey();
             }
-            Console.ReadKey();
         }
-        static void intializeGame()
+        /// <summary>
+        /// Lets main user select if they want to play or quit the game.
+        /// </summary>
+        static void GameMenu() 
+        {
+            Console.WriteLine("Välkommen till 21:an!");
+            Console.WriteLine("Välj ett alternativ");
+            Console.WriteLine("1. Spela 21:an");
+            Console.WriteLine("2. Avsluta spelet");
+            Console.Write("Skriv ditt alternativ: ");
+            bool choice = int.TryParse(Console.ReadLine(), out selectedMenuInput);
+            while(!choice)
+            {
+                Console.WriteLine("Felaktigt svar!");
+                Console.Write("Skriv ditt alternativ: ");
+                choice = int.TryParse(Console.ReadLine(), out selectedMenuInput);
+            }
+            switch(selectedMenuInput)
+            {
+                case 1:
+                    break;
+                case 2:
+                    exitGame = true;
+                    break;
+            }  
+        }
+        /// <summary>
+        /// Asks users to create new players until they feel satisfied, breaks out of loop.
+        /// Gives each player 2 random rands including the dealer
+        /// Prints every user's amount of cards.
+        /// </summary>
+        static void IntializeGame()
         {
             while (playerNotDone == true)
             {
+                Console.WriteLine("");
                 Console.WriteLine("Lägg till spelare?");
                 Console.WriteLine("1: Ja");
                 Console.WriteLine("2: Nej");
-                Console.Write("");
-                int choice = int.Parse(Console.ReadLine());
-                switch (choice)
+                Console.Write("Skriv ditt alternativ: ");
+                bool choice = int.TryParse(Console.ReadLine(), out selectedMenuInput);
+                while (!choice)
+                {
+                    Console.WriteLine("Felaktigt svar!");
+                    Console.Write("Skriv ditt alternativ: ");
+                    choice = int.TryParse(Console.ReadLine(), out selectedMenuInput);
+                }
+                switch (selectedMenuInput)
                 {
                     case 1:
                         Console.Write("Name: ");
                         string name = Console.ReadLine();
-                        Player currentPlayer = new Player();
+                        Player currentPlayer = new Player(); 
                         currentPlayer.Name = name;
                         players.Add(currentPlayer);
                         break;
                     case 2:
                         playerNotDone = false;
                         break;
+                    default:
+                        Console.WriteLine("Felaktigt svar, försök igen!");
+                        break;
                 }
             }
 
             for (int i = 0; i < 2; i++)
             {
-                dealerCards += randomInt.Next(1, 10);
+                dealerCards += randomInt.Next(1, 11);
 
             }
             Console.WriteLine("");
@@ -64,53 +112,74 @@ namespace Blackjack
                     p.playerCards += randomInt.Next(1, 11);
 
                 }
-                Console.WriteLine("{0} Din poäng: {1}", p.Name, p.playerCards);
+                Console.WriteLine("[{0}] Poäng: {1}", p.Name, p.playerCards);
             }
-            Console.WriteLine("");
 
         }
+        /// <summary>
+        /// Goes through list of players one by one, deals new cards until player is satisfied
+        /// Player loses if sum of all cards goes above 21.
+        /// When all players are satisfied, end game with function dealerDeal()
+        /// </summary>
         static void playerDeal()
         {
             int i = 0;
             foreach (Player p in players)
             {
+                Console.WriteLine("");
                 Console.WriteLine("Playing as player {0}", p.Name);
                 playerNotDone = true;
                 while (playerNotDone == true)
                 {
-                    Console.Write("Vill du ta ett nytt kort? (Ja)");
-                    string prompt = Console.ReadLine();
-                    if (prompt.ToLower() == "ja")
+                    Console.WriteLine("");
+                    Console.WriteLine("Vill du ta ett nytt kort?");
+                    Console.WriteLine("1: Ja");
+                    Console.WriteLine("2: Nej");
+                    Console.Write("Skriv ditt alternativ: ");
+                    bool choice = int.TryParse(Console.ReadLine(), out selectedMenuInput);
+                    while(!choice)
                     {
-
-                        int newCard = randomInt.Next(1, 11);
-                        p.playerCards += newCard;
-
-                        Console.WriteLine("Du drog en {0}, du har nu {1} poäng", newCard, p.playerCards);
-                        Console.WriteLine("Datorns poäng: {0}", dealerCards);
-                        if (p.playerCards > 21)
-                        {
-                            p.playerCards = -1;
-                            Console.WriteLine("You lost, sorry!");
-                            playerNotDone = false;
-                        }
+                        Console.WriteLine("Felaktigt svar!");
+                        Console.Write("Skriv ditt alternativ: ");
+                        choice = int.TryParse(Console.ReadLine(), out selectedMenuInput);
                     }
-                    else
+                    switch(selectedMenuInput)
                     {
-                        playerNotDone = false;
-                        break;
+                        case 1:
+                            int newCard = randomInt.Next(1, 11);
+                            p.playerCards += newCard;
+                            Console.WriteLine("Du drog en {0}, du har nu {1} poäng", newCard, p.playerCards);
+                            Console.WriteLine("Datorns poäng: {0}", dealerCards);
+                            if (p.playerCards > 21)
+                            {
+                                p.playerCards = -1;
+                                Console.WriteLine("You lost, sorry!");
+                                Console.WriteLine("");
+                                playerNotDone = false;
+                            }
+                            break;
+                        case 2:
+                            playerNotDone = false;
+                            break;
                     }
                 }
                 i++;
             }
             maxNum = players.Max(player => player.playerCards);
         }
-
+        /// <summary>
+        /// Dealer tries to surpass/tie with player that had highest number not over 21.
+        /// If dealer is above every remaining player AND not over 21, dealer wins.
+        /// If there are no players, dealer wins.
+        /// If dealer goes over 21, remaining players win.
+        /// If dealer is still under highest player not over 21, dealer draws a new card.
+        /// </summary>
         private static void dealerDeal()
         {
             dealerNotDone = true;
             while (dealerNotDone == true)
             {
+                Console.WriteLine("");
                 if (dealerCards == maxNum)
                 {
                     Console.WriteLine("Tie!");
@@ -122,7 +191,7 @@ namespace Blackjack
                 }
                 else if (dealerCards > 21)
                 {
-                    Console.WriteLine("You won!");
+                    Console.WriteLine("Remaining players win!");
                     dealerNotDone = false;
                 }
                 else if (dealerCards < maxNum)
